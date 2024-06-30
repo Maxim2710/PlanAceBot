4fc6e34105a170fa922994257c70c3b2f92001d4
PrimakMV <pm2710@mail.ru>
2024-06-30 16:43:45 +0300

    Initial commit

---
+++ src/main/java/com/TaskPlannerBot/TaskPlannerBotApplication.java
@@ -0,0 +1,13 @@
+package com.TaskPlannerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TaskPlannerBot/TaskPlannerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskPlannerBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
